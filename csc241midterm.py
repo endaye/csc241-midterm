663e967b61ec4a10a8c0595037d585665650fc33
Yuancheng Zhang <old.far.hot@gmail.com>
2015-02-26 03:12:26 -0600

    q3

--- csc241midterm.py
+++ csc241midterm.py
@@ -16,12 +16,26 @@ def countPos(lst):
             count = 0
     return count
 # Q1 test
-print (countPos([1,3,5,-5,-3,-1]))
+print ("--- Q1 test ---")
+# test 1
+print (">>> countPos([1, 3, 5, -5, -3, -1])")
+print (countPos([1, 3, 5, -5, -3, -1]))
+# test 2
+print (">>> num = countPos([6, 4, 2, 0])")
+print (">>> num")
 num = countPos([6, 4, 2, 0])
 print (num)
+# test 3
+print (">>> countPos([3.1415, 2.178, -3.333, 17.159, 0.11])")
 print (countPos([3.1415, 2.178, -3.333, 17.159, 0.11]))
+# test 4
+print (">>> countPos([-1, -2, -3])")
 print (countPos([-1, -2, -3]))
+# test 5
+print (">>> countPos([])")
 print (countPos([]))
+print (">>> \n")
+
 
 # Question 2
 def censorNum(s):
@@ -34,40 +48,65 @@ def censorNum(s):
         s1 = s1 + i
     return(s1)
 # Q2 test
-print (censorNum('1,2,3,blast off!'))
+print ("--- Q2 test ---")
+# test 1
+print (">>> censorNum('1, 2, 3, blast off!')")
+print (censorNum('1, 2, 3, blast off!'))
+# test 2
+print ('>>> censorNum("My favourite number is 17.")')
 print (censorNum("My favourite number is 17."))
+# test 3
+print ('>>> censorNum("This is a test. This is only a test.")')
 print (censorNum("This is a test. This is only a test."))
+# test 4
+print (">>> censorNum('')")
 print (censorNum(''))
+# test 5
+print ('>>> censorNum("12345")')
 print (censorNum("12345"))
-"""            
+print ('>>>> \n')
+
+
+         
 # Question 3
 def apr(info):
-    for i in range(len(info)):
-        if info[i] == ' ' and info[i+1] in '0123456789':
-            no = info[i+1]+info[i+2]+info[i+3]
-        if (info[-1:]=='A' or info[-1:]=='a' or info[-1:]=='B' or info[-1:]=='b' 
-            or info[-2:]=='A+' or info[-2:]=='a+' or info[-2:]=='A-' or info[-2:]=='a-'
-            or info[-2:]=='B+' or info[-2:]=='b+' or info[-2:]=='b-' or info[-2:]=='B-'):
-            print('Congratulations, you are doing well in',info[0:3],no+'!')
+    print(info.split())
+    name = info.split()[0]
+    num = info.split()[1]
+    grade = info.split()[2]
+    lv = grade[0].upper()
+    if lv in ('A','B') :
+        print ('Congratulations, you are doing well in', name, num + '!')
+    elif lv in ('C') :
+        print('You are passing',name, num + '.')
+    elif lv in ('D', 'F') :
+        print('You need to improve your performance in', name, num + '.')
+    else :
+        print('An invalid grade for', name, num, 'was provided.')
 
-        elif (info[-1:]=='C' or info[-1:]=='c' or info[-2:]=='C+' or info[-2:]=='c+' 
-            or info[-2:]=='C-' or info[-2:]=='c-'):
-            print('You are passing',info[0:3],no+'.')
+# Q3 test
+print ("--- Q3 test ---")
+# test 1
+#print(">>> apr('CSC  241    A-')")
+apr('CSC      241    A-')
+# test 2
+#print ('>>> apr("CSC  242    c")')
+apr("CSC  242    c")
+# test 3
+#print ('>>> apr("CSC     373       d+")')
+apr("CSC     373       d+")
+# test 4
+#print (">>> apr('WRD 104 B+')")
+apr('WRD 104 B+')
+# test 5
+#print (">>> apr('IT 130    f')")
+apr('IT    130    f')
+# test 6
+print (">>> apr('CSC 241 E')")
+apr('CSC 241 E')
+print (">>> \n")
 
-        elif(info[-1:]=='D' or info[-1:]=='d' or info[-1:]=='F' or info[-1:]=='f' 
-            or info[-2:]=='D+' or info[-2:]=='d+' or info[-2:]=='D-' or info[-2:]=='d-'):
-            print('You need to improve your performance in',info[0:3],no+'.')
 
-        else:
-            print('An invalid grade for',info[0:3],no,'was provided.')
-
-print(apr('CSC      241    A-'))
-print(apr("CSC  242    c"))
-print(apr("CSC     373       d+"))
-print(apr('WRD 104 B+'))
-print(apr('IT    130    f'))
-print(apr('CSC 241 E'))
-"""
 # Question 4
 def sentenceCount(fname):
     inf=open(fname,'r')
@@ -86,8 +125,17 @@ def sentenceCount(fname):
         j = i
     return (count)
 
+# Q4 test
+print ("--- Q4 test ---")
+# test 1
+print (">>> sentenceCount('csc241midterm/test1.txt')")
 print (sentenceCount('csc241midterm/test1.txt'))
+# test 2
+print (">>> num = sentenceCount('csc241midterm/test2.txt')")
+print (">>> num")
 num = sentenceCount('csc241midterm/test2.txt')
 print (num)
+# test 3
+print (">>> sentenceCount('csc241midterm/empty.txt')")
 print (sentenceCount('csc241midterm/empty.txt'))
-
+print (">>> \n")
